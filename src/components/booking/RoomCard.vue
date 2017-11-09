7864133cefc772054cc3e4838e8c06332d57e495
hilton <hong304@gmail.com>
2017-11-09 19:00:14 +0800

    reservation

--- src/components/booking/RoomCard.vue
+++ src/components/booking/RoomCard.vue
@@ -1,79 +1,88 @@
 <template>
-	<div class="room-card row">
-		<div class="image-thumb-wrapper col-md-5 col-xs-12">
-			<img :src="imageSrc"/>
-		</div>
-		<div class="info-wrapper col-md-7 col-xs-12">
-			<div class="content-wrapper">
-				<h3>{{ infoTitle }}</h3>
-				<h4>{{ infoPrice }}</h4>
-				<ul class="icon-list">
-					<li></li>
-				</ul>
-				<div class="rooms-extra">
-					<div class="extra-breakfast">
-						<div><multiselect
-								v-model="selected"
-								:options="options"
-								:searchable="false"
-								:close-on-select="true"
-								:showLabels="false"
-								:disabled="!counter"
-						></multiselect></div>
-						<div>
-							extra breakfast (MYR12 per night)<br>
-							<span>*max 1 extra breakfast per room</span>
-						</div>
-					</div>
-					<div class="extra-mattress">
-						<div><multiselect
-								v-model="selected"
-								:options="options"
-								:searchable="false"
-								:close-on-select="true"
-								:showLabels="false"
-								:disabled="!counter"
-						></multiselect></div>
-						<div>
-							extra mattress (MYR18 per night)<br>
-							<span>*max 1 extra mattress per room</span>
-						</div>
-					</div>
-				</div>
-				<div class="rooms-footer">
+    <div class="room-card row">
+        <div class="image-thumb-wrapper col-md-5 col-xs-12">
+            <img :src="imageSrc"/>
+        </div>
+        <div class="info-wrapper col-md-7 col-xs-12">
+            <div class="content-wrapper">
+                <h3 v-html="result['name_'+$i18n.locale]"></h3>
+                <h4>{{ result.price }}</h4>
+                <ul class="icon-list">
+                    <li></li>
+                </ul>
+                <div class="rooms-extra">
+                    <div class="extra-breakfast">
+                        <div>
+                            <multiselect
+                                    v-model="breakfast"
+                                    :options="options"
+                                    :searchable="false"
+                                    :close-on-select="true"
+                                    :showLabels="false"
+                                    :disabled="!counter"
+                            ></multiselect>
+                        </div>
+                        <div>
+                            extra breakfast (MYR12 per night)<br>
+                            <span>*max 1 extra breakfast per room</span>
+                        </div>
+                    </div>
+                    <div class="extra-mattress">
+                        <div>
+                            <multiselect
+                                    v-model="mattress"
+                                    :options="options"
+                                    :searchable="false"
+                                    :close-on-select="true"
+                                    :showLabels="false"
+                                    :disabled="!counter"
+                            ></multiselect>
+                        </div>
+                        <div>
+                            extra mattress (MYR18 per night)<br>
+                            <span>*max 1 extra mattress per room</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="rooms-footer">
 					<span>
 						{{ availableRooms }} rooms available |
 						<button type="button" @click="show=!show" class="btn btn-text-only">{{ buttonText }}</button>
 					</span>
-					<span class="rooms-number">
-						<button type="button" @click="counter -= 1" class="btn btn-minus" :disabled="!counter"><span class="ti-minus"></span></button>
+                    <span class="rooms-number">
+						<button type="button" @click="totalRooms(result.id)" class="btn btn-minus" :disabled="!counter"><span
+                                class="ti-minus"></span></button>
 						<span class="counter-num">{{ counter }}</span>
-						<button type="button" @click="counter += 1" class="btn btn-plus"><span class="ti-plus"></span></button>
+                        <!--<input v-model="no_of_rooms[result.id]">-->
+						<button type="button" @click="totalRooms(result.id)" class="btn btn-plus"><span
+                                class="ti-plus"></span></button>
 					</span>
-				</div>
-			</div>
-			<collapse v-model="show">
-				<div class="well" style="margin-bottom: 0">
-					<h5>The Room</h5>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-					<h5>Amenities</h5>
-					<ul class="icon-list">
-						<li></li>
-					</ul>
-					<h5>House Rules</h5>
-					<ul class="info-list">
-						<li></li>
-					</ul>
-					<h5>Cancellations</h5>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-					<h5>Safety features</h5>
-					<ul class="info-list">
-						<li></li>
-					</ul>
-				</div>
-			</collapse>
-		</div>
-	</div>
+                </div>
+            </div>
+            <collapse v-model="show">
+                <div class="well" style="margin-bottom: 0">
+                    <h5>The Room</h5>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <h5>Amenities</h5>
+                    <ul class="icon-list">
+                        <li></li>
+                    </ul>
+                    <h5>House Rules</h5>
+                    <ul class="info-list">
+                        <li></li>
+                    </ul>
+                    <h5>Cancellations</h5>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <h5>Safety features</h5>
+                    <ul class="info-list">
+                        <li></li>
+                    </ul>
+                </div>
+            </collapse>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -85,28 +94,11 @@
       Multiselect
     },
     props: {
+      result: {type: Object},
       imageSrc: {
         type: String,
         default: function () {
           return '/static/img/demo-room.jpg'
-        }
-      },
-      infoTitle: {
-        type: String,
-        default: function () {
-          return 'Default title'
-        }
-      },
-      infoPrice: {
-        type: String,
-        default: function () {
-          return '1,999'
-        }
-      },
-      selected: {
-        type: Number,
-        default: function () {
-          return 0
         }
       },
       options: {
@@ -132,121 +124,137 @@
     },
     data () {
       return {
-        counter: 0
+        counter: 0,
+        info: {},
+        no_of_rooms: [],
+        mattress: 0,
+        breakfast: 0
       }
-    }
+    },
+    methods: {
+      totalRooms (typeId) {
+        this.counter++
+//        this.no_of_rooms[typeId]['breakfast'] = 1
+        console.log(this.no_of_rooms)
+      }
+    },
+    computed: {}
   }
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style lang="scss" scoped>
-	@import '../../assets/style/setting';
+    @import '../../assets/style/setting';
 
-	.rooom-card {
-		border-bottom: 2px solid $brand-primary;
-		padding-bottom: 2.5rem;
-		margin-bottom: 2.5rem;
-	}
-	.image-thumb-wrapper {
-		& > img {
-			width: 100%;
-		}
-	}
+    .rooom-card {
+        border-bottom: 2px solid $brand-primary;
+        padding-bottom: 2.5rem;
+        margin-bottom: 2.5rem;
+    }
 
-	.info-wrapper {
-		text-align: left;
-		color: $brand-secondary;
-		.content-wrapper {
-			& > h3, & > h4 {
-				margin: 0;
-				text-transform: uppercase;
-			}
-			& > h3 {
-				font-weight: bold;
-			}
-			& > h4 {
-				font-weight: 100;
-				margin-bottom: 1rem;
-			}
-			.rooms-extra {
-				.extra-breakfast {
-					margin-bottom: 0.5rem;
-				}
-				.extra-breakfast, .extra-mattress {
-					& > div {
-						display: inline-block;
-						line-height: 20px;
-						&:first-of-type {
-							float: left;
-						}
-						&:last-of-type {
-							width: calc(100% - 60px);
-							& > span {
-								color: $brand-primary;
-								font-weight: bold;
-								font-style: italic;
-							}
-						}
-					}
-				}
-			}
-			.rooms-footer {
-				margin-top: 1.5rem;
-				margin-bottom: 1.5rem;
-			}
-		}
-		.btn-text-only {
-			color: $brand-primary;
-			padding: 0.5rem 0;
-			&:hover, &:focus {
-				color: $brand-secondary;
-			}
-		}
-		.btn-minus, .btn-plus {
-			background-color: transparent;
-			color: $brand-primary;
-			width: 25px;
-			height: 25px;
-			border: 1px solid $brand-primary;
-			border-radius: 50%;
-			padding: 0;
-			line-height: 25px;
-			font-size: 15px;
-			vertical-align: middle;
-			outline: none;
-		}
-		.counter-num {
-			font-size: 2rem;
-			line-height: 25px;
-			vertical-align: middle;
-		}
-		.rooms-number {
-			float: right;
-		}
-	}
-	.well {
-		border: none;
-		border-radius: 0;
-		box-shadow: none;
-		& > h5 {
-			font-weight: bold;
-			margin-top: 2rem;
-			margin-bottom: 0.5rem;
-			font-size: 1.5rem;
-			&:first-of-type {
-				margin-top: 0;
-			}
-		}
-	}
-	ul {
-		list-style-type: none;
-		padding-left: 0;
-	}
-	.multiselect {
-		display: inline-block;
-		width: 50px;
-		min-height: 30px;
-		margin-right: 1rem;
-		color: $brand-secondary;
-	}
+    .image-thumb-wrapper {
+        & > img {
+            width: 100%;
+        }
+    }
+
+    .info-wrapper {
+        text-align: left;
+        color: $brand-secondary;
+        .content-wrapper {
+            & > h3, & > h4 {
+                margin: 0;
+                text-transform: uppercase;
+            }
+            & > h3 {
+                font-weight: bold;
+            }
+            & > h4 {
+                font-weight: 100;
+                margin-bottom: 1rem;
+            }
+            .rooms-extra {
+                .extra-breakfast {
+                    margin-bottom: 0.5rem;
+                }
+                .extra-breakfast, .extra-mattress {
+                    & > div {
+                        display: inline-block;
+                        line-height: 20px;
+                        &:first-of-type {
+                            float: left;
+                        }
+                        &:last-of-type {
+                            width: calc(100% - 60px);
+                            & > span {
+                                color: $brand-primary;
+                                font-weight: bold;
+                                font-style: italic;
+                            }
+                        }
+                    }
+                }
+            }
+            .rooms-footer {
+                margin-top: 1.5rem;
+                margin-bottom: 1.5rem;
+            }
+        }
+        .btn-text-only {
+            color: $brand-primary;
+            padding: 0.5rem 0;
+            &:hover, &:focus {
+                color: $brand-secondary;
+            }
+        }
+        .btn-minus, .btn-plus {
+            background-color: transparent;
+            color: $brand-primary;
+            width: 25px;
+            height: 25px;
+            border: 1px solid $brand-primary;
+            border-radius: 50%;
+            padding: 0;
+            line-height: 25px;
+            font-size: 15px;
+            vertical-align: middle;
+            outline: none;
+        }
+        .counter-num {
+            font-size: 2rem;
+            line-height: 25px;
+            vertical-align: middle;
+        }
+        .rooms-number {
+            float: right;
+        }
+    }
+
+    .well {
+        border: none;
+        border-radius: 0;
+        box-shadow: none;
+        & > h5 {
+            font-weight: bold;
+            margin-top: 2rem;
+            margin-bottom: 0.5rem;
+            font-size: 1.5rem;
+            &:first-of-type {
+                margin-top: 0;
+            }
+        }
+    }
+
+    ul {
+        list-style-type: none;
+        padding-left: 0;
+    }
+
+    .multiselect {
+        display: inline-block;
+        width: 50px;
+        min-height: 30px;
+        margin-right: 1rem;
+        color: $brand-secondary;
+    }
 </style>
--- src/views/Reservations.vue
+++ src/views/Reservations.vue
@@ -1,26 +1,26 @@
 <template>
-	<div class="container" id="reservations">
-		<section class="mt-5 py-5">
-			<div class="row">
-				<div class="col-xs-12">
-					<content-title :contentTitle="$t('pages.reservations.pageTitle')"></content-title>
-					<content-paragraph></content-paragraph>
-				</div>
-			</div>
-		</section>
-		<section class="py-5">
-			<div class="row">
-				<div class="col-md-8 col-xs-12">
-					<room-card></room-card>
-					<room-card></room-card>
-					<room-card></room-card>
-				</div>
-				<div class="col-md-4 col-xs-12">
-					<booking-sticky :isMobile="isMobile"></booking-sticky>
-				</div>
-			</div>
-		</section>
-	</div>
+    <div class="container" id="reservations">
+        <section class="mt-5 py-5">
+            <div class="row">
+                <div class="col-xs-12">
+                    <content-title :contentTitle="$t('pages.reservations.pageTitle')"></content-title>
+                    <content-paragraph></content-paragraph>
+                </div>
+            </div>
+        </section>
+        <section class="py-5">
+            <div class="row">
+                <div class="col-md-8 col-xs-12">
+                    <div v-for="(item, index) in roomTypes">
+                        <room-card :result="item"></room-card>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <booking-sticky :isMobile="isMobile"></booking-sticky>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
@@ -40,7 +40,7 @@
     data () {
       return {
         titleOne: 'Reservations',
-        rooms: []
+        roomTypes: []
       }
     },
     props: {
@@ -49,7 +49,7 @@
     methods: {
       fetchRooms: function () {
         this.axios.get('/api/room-type').then((response) => {
-          this.rooms = response.data
+          this.roomTypes = response.data
           console.log(response.data)
         }, (error) => {
           console.log(error)
@@ -64,21 +64,21 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	h1, h2 {
-		font-weight: normal;
-	}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-	ul {
-		list-style-type: none;
-		padding: 0;
-	}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-	li {
-		display: inline-block;
-		margin: 0 10px;
-	}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-	a {
-		color: #42b983;
-	}
+    a {
+        color: #42b983;
+    }
 </style>

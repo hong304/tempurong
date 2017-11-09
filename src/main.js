ecd69ef4b49b7d4ff798c0262b97cf7cb19211e9
hilton <hong304@gmail.com>
2017-11-09 11:20:05 +0800

    sass-loader

--- src/main.js
+++ src/main.js
@@ -10,12 +10,15 @@ import router from './router/index'
 import VueI18n from 'vue-i18n'
 import messages from './i18n/locale'
 import * as uiv from 'uiv'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 window._ = require('lodash')
 
 Vue.config.productionTip = false
 Vue.use(VueI18n)
 Vue.use(uiv)
+Vue.use(VueAxios, axios)
 
 const i18n = new VueI18n({
   locale: 'en',
--- src/views/Reservations.vue
+++ src/views/Reservations.vue
@@ -1,26 +1,26 @@
 <template>
-	<div class="container" id="reservations">
-		<section class="mt-5 py-5">
-			<div class="row">
-				<div class="col-xs-12">
-					<content-title :contentTitle="titleOne"></content-title>
-					<content-paragraph></content-paragraph>
-				</div>
-			</div>
-		</section>
-		<section class="py-5">
-			<div class="row">
-				<div class="col-md-8 col-xs-12">
-					<room-card></room-card>
-					<room-card></room-card>
-					<room-card></room-card>
-				</div>
-				<div class="col-md-4 col-xs-12">
-					<booking-sticky :isMobile="isMobile"></booking-sticky>
-				</div>
-			</div>
-		</section>
-	</div>
+    <div class="container" id="reservations">
+        <section class="mt-5 py-5">
+            <div class="row">
+                <div class="col-xs-12">
+                    <content-title :contentTitle="titleOne"></content-title>
+                    <content-paragraph></content-paragraph>
+                </div>
+            </div>
+        </section>
+        <section class="py-5">
+            <div class="row">
+                <div class="col-md-8 col-xs-12">
+                    <room-card></room-card>
+                    <room-card></room-card>
+                    <room-card></room-card>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <booking-sticky :isMobile="isMobile"></booking-sticky>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
@@ -39,32 +39,46 @@
     name: 'Reservations',
     data () {
       return {
-        titleOne: 'Reservations'
+        titleOne: 'Reservations',
+        rooms: []
       }
     },
     props: {
       isMobile: this.isMobile
+    },
+    methods: {
+      fetchRooms: function () {
+        this.axios.get('/api/room').then((response) => {
+          this.rooms = response.data
+          console.log(response.data)
+        }, (error) => {
+          console.log(error)
+        })
+      }
+    },
+    mounted: function () {
+      this.fetchRooms()
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	h1, h2 {
-		font-weight: normal;
-	}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-	ul {
-		list-style-type: none;
-		padding: 0;
-	}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-	li {
-		display: inline-block;
-		margin: 0 10px;
-	}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-	a {
-		color: #42b983;
-	}
+    a {
+        color: #42b983;
+    }
 </style>

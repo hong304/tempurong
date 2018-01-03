f0fe9c8bea50d69ac60357d0fb9acb01a316ee48
hilton <hong304@gmail.com>
2018-01-03 15:56:04 +0800

    Merge branch 'staging' of https://github.com/cjmartino/tempurong into staging
    # Conflicts:
    #	src/views/Reservations/Summary.vue

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://api.tempurong.buildonauts.com"'
+  API_URL: '"http://localhost:9000"'
 })
